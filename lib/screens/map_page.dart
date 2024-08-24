4f9f4dcbcdb4ae27770cf825991f98ec49fc722f
prabashwara65 <prabashwara65@gmail.com>
2024-08-24 17:30:48 +0530

    project commit

---
+++ lib/screens/map_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:permission_handler/permission_handler.dart'; // Add this package to pubspec.yaml
+
+class MapPage extends StatefulWidget {
+  const MapPage({super.key});
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  static const LatLng _pGooglePlex = LatLng(37.4223, -122.0848);
+  late GoogleMapController mapController;
+
+  @override
+  void initState() {
+    super.initState();
+    _requestLocationPermission(); // Request location permission on initialization
+  }
+
+  Future<void> _requestLocationPermission() async {
+    var status = await Permission.locationWhenInUse.status;
+    if (status.isDenied) {
+      await Permission.locationWhenInUse.request();
+    }
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Google Map Example')),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: const CameraPosition(
+          target: _pGooglePlex,
+          zoom: 13,
+        ),
+        myLocationEnabled: true, // This enables the MyLocation layer
+        myLocationButtonEnabled: true,
+      ),
+    );
+  }
+}

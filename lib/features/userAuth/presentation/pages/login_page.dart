00a0abb1f4b1f26de125fefe7423cc8332906d4f
prabashwara65 <prabashwara65@gmail.com>
2024-08-27 22:13:24 +0530

    widget components

--- lib/features/userAuth/presentation/pages/login_page.dart
+++ lib/features/userAuth/presentation/pages/login_page.dart
@@ -2,9 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_firebase/features/user_auth/presentation/pages/sign_up_page.dart';
-import 'package:flutter_firebase/features/user_auth/presentation/widgets/form_container_widget.dart';
-import 'package:flutter_firebase/global/common/toast.dart';
+import 'package:flutter_map/features/user_auth/presentation/pages/sign_up_page.dart';
+import 'package:flutter_map/features/userAuth/presentation/widgets/form_container_widget.dart';
+import 'features/userAuth/presentation/widgets/form_container_widget.dart';
+import 'package:flutter_map/global/common/toast.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 

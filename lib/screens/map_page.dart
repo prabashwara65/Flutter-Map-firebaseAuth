e2de373b0ebff573df01846d6b51134e2581c026
prabashwara65 <prabashwara65@gmail.com>
2024-09-09 10:44:06 +0530

    removed unnessasery files

--- lib/screens/map_page.dart
+++ lib/screens/map_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:permission_handler/permission_handler.dart'; // Add this package to pubspec.yaml
+
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});

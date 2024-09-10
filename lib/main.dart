41e14f2eddcb6c21e036aaab00dd25e69a950183
prabashwara65 <prabashwara65@gmail.com>
2024-09-10 16:23:33 +0530

    removed firebase files

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-// import 'package:flutter_map/features/userAuth/presentation/pages/home_page.dart';
 import 'package:flutter_map/screens/map_page.dart';
-
-//platform exception has been occured.it must be fix
-//firebase dosen't work properly 
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();  // Initialize Firebase
+  // await Firebase.initializeApp();  // Initialize Firebase
   runApp(MyApp());
 }
 
@@ -22,7 +17,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       //Start initial page
-      //home: HomePage(),
       home: MapPage(),
     );
   }
